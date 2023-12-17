c8069634211fdb3c9937e2781cb7910e5c4883c6
arbazm10 <arbazmtn@gmail.com>
2023-12-17 17:56:59 +0000

    update 7

--- terraform-manifests/main.tf
+++ terraform-manifests/main.tf
@@ -1,13 +1,8 @@
 terraform {
-
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "~>3.8.0"
-    }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.0"
     }
     random = {
       source  = "hashicorp/random"
