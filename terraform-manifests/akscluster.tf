b26b187f71f7eb1bfbc7736faadafa1c59b9491c
arbazm10 <123230684+arbazm10@users.noreply.github.com>
2023-12-18 11:00:06 +0530

    Update akscluster.tf

--- terraform-manifests/akscluster.tf
+++ terraform-manifests/akscluster.tf
@@ -2,7 +2,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = "${azurerm_resource_group.aks_rg.name}-cluster"
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
-  dns_prefix          = "${azurerm_resource_group.aks_rg.name}-cluster"
+  dns_prefix          = "${azurerm_resource_group.aks_rg.name}"
   kubernetes_version  = data.azurerm_kubernetes_service_versions.current.latest_version
   node_resource_group = "${azurerm_resource_group.aks_rg.name}-nrg"
 
@@ -85,7 +85,7 @@ azure_active_directory_role_based_access_control {
   }
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment
   }
 }
 
